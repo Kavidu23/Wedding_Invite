61fddb51ea393db5cf1aa82d141f5a24ec85d030
Kavidu Lakshan <10952412@students.plymouth.ac.uk>
2024-10-17 20:38:39 +0530

    Add files via upload

---
+++ RSVP.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wecome To - Our World</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montaga&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="header">
+        <a href="#"><b>Liam & Olivia</b></a>
+        <a href="#">RSVP</a>
+        <a href="#">Our Story</a>
+    </div>
+    <form>
+        <label>Name</label>
+        <input type="text" name="name" class="name" placeholder="Name">
+        <label>E-mail</label>
+        <input type="mail" name="mail" class="mail" placeholder="E-mail">
+        <label>Will You Be Able To Attend ?</label>
+        <div class="preference">
+        Yes: <input type="radio" value="yes" name="pre">
+        No:  <input type="radio" value="No" name="pre">
+        </div>
+        <label>Message</label>
+        <textarea placeholder="Message" cols="40"></textarea>
+        <button>Submit</button>
+    </div>
+    </form>
+    <div class="footer">
+        <div class="template">
+            <a href="#">Aaron Rudyk - Design</a>
+        </div>
+        <div class="fornt_end">
+            <a href="#">Kavidu Lankshan - Code</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wecome To - Our World</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montaga&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="header">
+        <a href="#"><b>Liam & Olivia</b></a>
+        <a href="RSVP.php">RSVP</a>
+        <a href="#">Our Story</a>
+    </div>
+    <div class="mid_body">
+        <span class="desc_one">
+             <p class="topic">Liam &<br> Olivia Are<br> Getting<br> Married !</p>
+             <p class="explain">We are so excited to celebrate our special day with all of<br> our favorite people.
+                 This website is your go-to resource for<br> all the details about our wedding and 
+                 the best place to<br> RSVP.
+             </p>
+             <button>RSVP</button>
+             <p class="endtopic">Please RSVP by January 1st</p>
+        </span>
+        <div class="image">
+             <img src="../Wedding_Site/Images/first.jpg" alt="couple_image" height="700px" width="500px">
+        </div>
+    </div>
+    <div class="sec_body">
+        <div class="image">
+             <img src="../Wedding_Site/Images/second.jpg" alt="couple_image" height="600px" width="500px">
+        </div>
+        <div class="info">
+            <p class="topic1">RSVP & Info</p>
+            <p class="desc">We're really excited to see you, and hope you can join us for our special day!</p>
+            <p class="topic2">When</p>
+            <p class="desc">Monady, July 8, 2026</p>
+            <p class="desc">5.30 PM - 12.30 PM</p>
+            <p class="topic3">Where</p>
+            <p class="desc">The Venue .. .. .. . . .</p>
+            <p class="topic4">Dress Code</p>
+            <p class="desc">Formal Attitude</p>
+            <p class="desc">Please let us know if you are able to make it before 20st july</p>
+            <button>RSVP</button>
+        </div>
+    </div>
+    <div class="third_body">
+        <div class="paragraph">
+            <p class="titlethird">Shedule</p>
+            <p class="textthird">We're really excited to see you, and hope you can join us for our special day!</p>
+        </div>
+        <section class="sectionone">
+        <div class="card" style="width: 15rem;">
+            <img src="../Wedding_Site/Images/wedding.jpg" class="card-img-top" height="150px" width="100px" alt="...">
+            <div class="card-body">
+            <p class="card_topic">Wedding Ceremony</p>    
+            <p class="card-text">We exchange our vows and tie the knot amidst the beauty of St Marry’s Church</p>
+            </div>
+        </div>
+        <div class="card" style="width: 15rem;">
+            <img src="../Wedding_Site/Images/drink.jpg" height="150px" width="100px" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card_topic">Appetizers & Drinks</p>     
+            <p class="card-text">Mingle over appetizers and refreshing drinks, setting the tone for an evening of celebration.</p>
+            </div>
+        </div>
+        <div class="card" style="width: 15rem;">
+            <img src="../Wedding_Site/Images/dinner.jpg" height="150px" width="100px" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card_topic">Dinner Service</p>     
+            <p class="card-text">A gourmet dinner service offering a culinary journey, tailored to indulge the senses and honor the occasion.</p>
+            </div>
+        </div>
+        <div class="card" style="width: 15rem;">
+            <img src="../Wedding_Site/Images/dj.jpg" height="150px" width="100px" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card_topic">Dance Party</p>     
+            <p class="card-text">Following the first dance we invite everyone to celebrate our love and joy through music and dance!</p>
+            </div>
+        </div>
+        </section>
+    </div>
+    <div class="footer">
+        <div class="template">
+            <a href="#">Aaron Rudyk - Design</a>
+        </div>
+        <div class="fornt_end">
+            <a href="#">Kavidu Lankshan - Code</a>
+        </div>
+    </div>
+</body>
+</html>
